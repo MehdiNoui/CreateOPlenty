55b8f1eb195fe3687e1667efbba90decfebc7636
Mehdi Noui <mehdinoui2004@gmail.com>
2025-09-21 19:19:22 +0100

    renamed files

--- src/main/java/net/mehdinoui/createoplenty/datagen/ItemModelProvider.java
+++ src/main/java/net/mehdinoui/createoplenty/datagen/ItemModelProvider.java
@@ -1,25 +1,24 @@
 package net.mehdinoui.createoplenty.datagen;
 
 import net.mehdinoui.createoplenty.CreateOPlenty;
-import net.mehdinoui.createoplenty.item.ModItems;
+import net.mehdinoui.createoplenty.item.COPItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 
-public class ModItemModelProvider extends ItemModelProvider {
-    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+public class ItemModelProvider extends net.minecraftforge.client.model.generators.ItemModelProvider {
+    public ItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
         super(output, CreateOPlenty.MOD_ID, existingFileHelper);
     }
 
     @Override
     protected void registerModels() {
-        simpleItem(ModItems.BLACK_SAND_PAPER);
-        simpleItem(ModItems.ORANGE_SAND_PAPER);
-        simpleItem(ModItems.WHITE_SAND_PAPER);
+        simpleItem(COPItems.BLACK_SAND_PAPER);
+        simpleItem(COPItems.ORANGE_SAND_PAPER);
+        simpleItem(COPItems.WHITE_SAND_PAPER);
     }
 
     private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
