857fbbf6356bee2533f57456fb17f7c88d90883b
Mehdi Noui <mehdinoui2004@gmail.com>
2025-09-22 21:55:11 +0100

    Compacting Recipes: - thermal_calcite_to_diorite - flesh_block_compact

---
+++ src/main/java/net/mehdinoui/createoplenty/data/recipes/COPCompactingRecipes.java
@@ -0,0 +1,42 @@
+package net.mehdinoui.createoplenty.data.recipes;
+
+import biomesoplenty.api.block.BOPBlocks;
+import biomesoplenty.api.block.BOPFluids;
+import com.simibubi.create.AllRecipeTypes;
+import com.simibubi.create.foundation.data.recipe.ProcessingRecipeGen;
+import com.simibubi.create.foundation.recipe.IRecipeTypeInfo;
+import net.mehdinoui.createoplenty.CreateOPlenty;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.Fluids;
+
+public class COPCompactingRecipes extends ProcessingRecipeGen {
+    public COPCompactingRecipes(PackOutput output) {
+        super(output);
+        // Thermal Calcite
+        create(
+                new ResourceLocation(CreateOPlenty.MOD_ID, "thermal_calcite_to_diorite"),
+                b -> b
+                        .require(Items.FLINT).require(Items.FLINT)
+                        .require(BOPBlocks.THERMAL_CALCITE)
+                        .require(Fluids.LAVA,100)
+                        .duration(350)
+                        .output(Blocks.DIORITE)
+        );
+        // Flesh Block
+        create(
+                new ResourceLocation(CreateOPlenty.MOD_ID, "flesh_block_compact"),
+                b -> b
+                        .require(Items.ROTTEN_FLESH).require(Items.ROTTEN_FLESH).require(Items.ROTTEN_FLESH).require(Items.ROTTEN_FLESH)
+                        .require(BOPFluids.BLOOD, 100)
+                        .duration(350)
+                        .output(BOPBlocks.FLESH)
+        );
+    }
+    @Override
+    protected AllRecipeTypes getRecipeType() {
+        return AllRecipeTypes.COMPACTING;
+    }
+}
