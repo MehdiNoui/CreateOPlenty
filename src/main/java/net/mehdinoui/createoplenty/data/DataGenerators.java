3da8da26a418a75e17a20ba036c996585125fb55
Mehdi Noui <mehdinoui2004@gmail.com>
2025-09-22 21:58:29 +0100

    Added the data generators

--- src/main/java/net/mehdinoui/createoplenty/data/DataGenerators.java
+++ src/main/java/net/mehdinoui/createoplenty/data/DataGenerators.java
@@ -1,6 +1,7 @@
-package net.mehdinoui.createoplenty.datagen;
+package net.mehdinoui.createoplenty.data;
 
 import net.mehdinoui.createoplenty.CreateOPlenty;
+import net.mehdinoui.createoplenty.data.recipes.*;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
@@ -13,13 +14,30 @@ import java.util.concurrent.CompletableFuture;
 
 @Mod.EventBusSubscriber(modid = CreateOPlenty.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
+
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-
-        generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
+        // Simple Item Model
+        generator.addProvider(event.includeClient(),
+                new ItemModelProvider(packOutput, existingFileHelper));
+        // Basic Recipes
+        generator.addProvider(event.includeClient(),
+                new COPBasicRecipes(packOutput));
+        // Compacting Recipes
+        generator.addProvider(event.includeServer(),
+                new COPCompactingRecipes(packOutput));
+        // Crushing Recipes
+        generator.addProvider(event.includeServer(),
+                new COPCrushingRecipes(packOutput));
+        // Milling Recipes
+        generator.addProvider(event.includeServer(),
+                new COPMillingRecipes(packOutput));
+        // Mixing Recipes
+        generator.addProvider(event.includeServer(),
+                new COPMixingRecipes(packOutput));
     }
 }
