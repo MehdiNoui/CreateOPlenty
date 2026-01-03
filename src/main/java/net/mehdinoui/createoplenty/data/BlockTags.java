07371fae4e8028f663ca0342d30355b17bd9f134
MehdiNoui <mehdinoui2004@gmail.com>
2026-01-03 19:01:40 +0100

    add annotations

--- src/main/java/net/mehdinoui/createoplenty/data/BlockTags.java
+++ src/main/java/net/mehdinoui/createoplenty/data/BlockTags.java
@@ -5,6 +5,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -14,6 +15,6 @@ public class BlockTags extends BlockTagsProvider {
     }
 
     @Override
-    protected void addTags(HolderLookup.Provider pProvider) {
+    protected void addTags(HolderLookup.@NotNull Provider pProvider) {
     }
 }
--- src/main/java/net/mehdinoui/createoplenty/data/ItemTags.java
+++ src/main/java/net/mehdinoui/createoplenty/data/ItemTags.java
@@ -11,6 +11,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.util.concurrent.CompletableFuture;
@@ -20,7 +21,7 @@ public class ItemTags extends ItemTagsProvider {
         super(output, provider, blockTagProvider, CreateOPlenty.MOD_ID, existingFileHelper);
     }
     @Override
-    protected void addTags(HolderLookup.Provider provider) {
+    protected void addTags(HolderLookup.@NotNull Provider provider) {
         this.tag(TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("create", "sandpaper")))
                 .add(COPItems.BLACK_SAND_PAPER.get())
                 .add(COPItems.ORANGE_SAND_PAPER.get())
