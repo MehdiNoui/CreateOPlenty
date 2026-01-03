56780d5c84d3da15e77aa1d2296443f3633efbcb
MehdiNoui <mehdinoui2004@gmail.com>
2026-01-04 00:49:15 +0100

    add 2 new sand compact recipes Diorite: Inputs: 2 Flint + 1 White Sand + 100 mB Lava Scrochia: Inputs: 2 Flint + 1 Black Sand + 100 mB Lava

--- src/main/java/net/mehdinoui/createoplenty/data/recipes/COPCompactingRecipes.java
+++ src/main/java/net/mehdinoui/createoplenty/data/recipes/COPCompactingRecipes.java
@@ -5,6 +5,7 @@ import biomesoplenty.api.block.BOPFluids;
 import com.simibubi.create.api.data.recipe.CompactingRecipeGen;
 import net.mehdinoui.createoplenty.CreateOPlenty;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
@@ -17,12 +18,23 @@ import java.util.concurrent.CompletableFuture;
 public class COPCompactingRecipes extends CompactingRecipeGen {
     public COPCompactingRecipes(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, CreateOPlenty.MOD_ID);
-        // Thermal Calcite
+        // Black Sand
         create(
-                ResourceLocation.fromNamespaceAndPath(CreateOPlenty.MOD_ID, "thermal_calcite_to_diorite"),
+                ResourceLocation.fromNamespaceAndPath(CreateOPlenty.MOD_ID, "black_sand_to_scrochia"),
                 b -> b
                         .require(Items.FLINT).require(Items.FLINT)
-                        .require(BOPBlocks.THERMAL_CALCITE)
+                        .require(BOPBlocks.BLACK_SAND)
+                        .require(Fluids.LAVA,100)
+                        .output(BuiltInRegistries.BLOCK.get(
+                                ResourceLocation.fromNamespaceAndPath("create", "scrochia")
+                        ))
+        );
+        // White Sand
+        create(
+                ResourceLocation.fromNamespaceAndPath(CreateOPlenty.MOD_ID, "white_sand_to_diorite"),
+                b -> b
+                        .require(Items.FLINT).require(Items.FLINT)
+                        .require(BOPBlocks.WHITE_SAND)
                         .require(Fluids.LAVA,100)
                         .output(Blocks.DIORITE)
         );
